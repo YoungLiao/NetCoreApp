b1b5248d0e9945a6023be2469c326f5ff2231c4c
YoungLiao <liao.brave@gmail.com>
2019-07-20 19:45:00 +0800

    demo

---
+++ WindowsService1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+
+namespace WindowsService1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var svc = new Service1();
+            svc.OnStart(args);
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
